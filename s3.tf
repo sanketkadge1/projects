089c501f9b16c24ae9eeddfee3cae019505a7216
sanketkadge1 <sanketkadge@gmail.com>
2022-07-21 19:58:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,104 @@
---try adding
+locals {
+  bucket_name = "s3-bucket-${random_pet.this.id}"
+}
+
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.this.arn]
+    }
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}",
+    ]
+  }
+}
+
+
+resource "aws_iam_role" "this" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_kms_key" "objects" {
+  description             = "KMS key is used to encrypt bucket objects"
+  deletion_window_in_days = 7
+}
+
+
+resource "random_pet" "this" {
+  length = 2
+}
+
+module "s3_bucket" {
+  source = "github.com/sanketkadge1/Terraform/tree/AWS/Modules/S3"
+
+  bucket = local.bucket_name
+
+  force_destroy       = true
+  acceleration_status = "Suspended"
+  request_payer       = "BucketOwner"
+
+  tags = {
+    name = "test"
+  }
+
+  acl = "private"
+  object_lock_enabled = true
+  object_lock_configuration = {
+    rule = {
+      default_retention = {
+        mode = "GOVERNANCE"
+        days = 1
+      }
+    }
+  }
+  attach_policy                         = true
+  policy                                = data.aws_iam_policy_document.bucket_policy.json
+
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = aws_kms_key.objects.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
