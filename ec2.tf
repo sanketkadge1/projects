4e759fe8b3a437c3857efd5a7c20fbb90a247f57
sanketkadge1 <sanketkadge@gmail.com>
2022-07-27 13:04:53 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+module "ec2_instance" {
+  source  = "git@github.com:karthickkumars/terraform-aws-modules//ec2?ref=master"
+  
+  name = "COP test"
+
+  ami                    = "ami-05803413c51f242b7"
+  instance_type          = "t2.micro"
+  # key_name               = "user1"
+  # monitoring             = true
+  # vpc_security_group_ids = ["sg-12345678"]
+  # subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+  Terraform   = "true"
+  Environment = "dev"
+  }
+}
