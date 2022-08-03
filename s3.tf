e19d7be0bca5fa5d1ba17488ba16bb29eb5b27bb
sanketkadge1 <sanketkadge@gmail.com>
2022-08-03 15:09:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,49 +3,6 @@ locals {
 }
 
 
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.this.arn]
-    }
-
-    actions = [
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${local.bucket_name}",
-    ]
-  }
-}
-
-
-resource "aws_iam_role" "this" {
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-
-resource "aws_kms_key" "objects" {
-  description             = "KMS key is used to encrypt bucket objects"
-  deletion_window_in_days = 7
-}
-
-
 resource "random_pet" "this" {
   length = 2
 }
@@ -64,35 +21,16 @@ module "s3_bucket" {
   }
 
   acl = "private"
-  object_lock_enabled = true
-  object_lock_configuration = {
-    rule = {
-      default_retention = {
-        mode = "GOVERNANCE"
-        days = 1
-      }
-    }
-  }
-  attach_policy                         = true
-  policy                                = data.aws_iam_policy_document.bucket_policy.json
-
+  object_lock_enabled = false
+  
   # S3 bucket-level Public Access Block configuration
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
   control_object_ownership = true
   object_ownership         = "BucketOwnerPreferred"
 
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.objects.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 
 
 }
