7aea306f2c73bd3237802db3da19732ead496334
sanketkadge1 <sanketkadge@gmail.com>
2022-08-03 15:17:10 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,11 @@ module "ec2_instance" {
 
   ami                    = "ami-05803413c51f242b7"
   instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg_jumpserver"] 
   key_name               = "kpCAIT"
   subnet_id              = "subnet-be7f88c4"
   availability_zone      = "us-east-2b"
+  
   tags = {
   Terraform   = "true"
   Environment = "dev"
