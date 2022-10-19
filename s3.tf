a4258068be07eadb5bc07a6f2b371d556fe1cbe7
sanketkadge1 <sanketkadge@gmail.com>
2022-10-19 08:26:59 +0000

    Update s3.tf
    Adding one more bucket

--- s3.tf
+++ s3.tf
@@ -36,7 +36,33 @@ module "s3_bucket" {
 }
 
 
+module "s3_bucket" {
+  source = "git@github.com:sanketkadge1/Terraform//Modules/S3?ref=AWS"
 
+  bucket = "democmtcop19102022"
+
+  force_destroy       = true
+  acceleration_status = "Suspended"
+  request_payer       = "BucketOwner"
+
+  tags = {
+    name = "test"
+  }
+
+  acl = "private"
+  object_lock_enabled = false
+  
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+
+
+}
 
 
 
